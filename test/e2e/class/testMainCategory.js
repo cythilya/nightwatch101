7c43a79439cce8339f6bd2a3061dc9b1594085d4
Cythilya Tang <cythilya@gmail.com>
2017-12-03 15:09:51 +0800

    [mod] add BDD expect example

--- test/e2e/class/testMainCategory.js
+++ test/e2e/class/testMainCategory.js
@@ -2,7 +2,13 @@ var conf = require('../../../nightwatch.conf.js');
 
 module.exports = {
   '@tags': ['class'],
-  'Demo Ruten MainCategory Page': browser => {
+  'Assert Ruten MainCategory Title': browser => {
+    browser.url('http://class.ruten.com.tw/category/main?0008');
+    browser.waitForElementVisible('body');
+    browser.expect.element('.rt-flagship .rt-ad-heading').text.to.equal('露天旗艦店');
+    browser.end();
+  },
+  'Demo Ruten Main Category Page': browser => {
     browser
       .url('http://class.ruten.com.tw/category/main?0008')
       .countElementNumber('a', function(result) {
