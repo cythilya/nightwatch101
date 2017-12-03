e3b9f8285a504b23db27c51b383ec8b9783f7003
Cythilya Tang <cythilya@gmail.com>
2017-12-03 08:52:41 +0800

    [add] add xpath example

---
+++ test/e2e/member/testLogin.js
@@ -0,0 +1,17 @@
+var conf = require('../../../nightwatch.conf.js');
+
+module.exports = {
+  'Ruten Login' : function (browser) {
+    browser
+    .url('https://member.ruten.com.tw/user/login.htm')
+    .waitForElementVisible('body', 1000)
+    .useXpath()
+    .setValue('//input[@id="userid"]', 'test_user') // test_user 要改為自己的帳號
+    .setValue('//input[@id="userpass"]', 'test_password') // test_password 要改為自己的密碼
+    .useCss()
+    .click('#btnLogin')
+    .pause(1000)
+    .assert.urlContains('http://www.ruten.com.tw/')
+    .end();
+  }
+};
