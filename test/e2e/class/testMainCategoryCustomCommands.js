19237263ca475e4079894786bbc1bf0a2b90982b
Cythilya Tang <cythilya@gmail.com>
2017-12-04 22:28:18 +0800

    [add] custom command example

---
+++ test/e2e/class/testMainCategoryCustomCommands.js
@@ -0,0 +1,13 @@
+var conf = require('../../../nightwatch.conf.js');
+
+module.exports = {
+  '@tags': ['class'],
+  'Demo Ruten Main Category Page': browser => {
+    browser
+      .url('http://class.ruten.com.tw/category/main?0008')
+      .countElementNumber('a', function(result) {
+        console.log(result.value);
+      })
+      .end()
+  }
+}
