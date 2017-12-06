7675f8c9ffd705304c1958d311cf4c6465cea9cf
Cythilya Tang <cythilya@gmail.com>
2017-12-06 15:20:35 +0800

    [add] add custom assertions example

---
+++ test/e2e/class/testMainCategoryCustomAssertions.js
@@ -0,0 +1,11 @@
+var conf = require('../../../nightwatch.conf.js');
+
+module.exports = {
+  '@tags': ['class'],
+  'Demo Ruten Main Category Page': browser => {
+    browser
+      .url('http://class.ruten.com.tw/category/sub00.php?c=00080001')
+      .assert.count('table tbody tr', 0)
+      .end()
+  }
+}
