db9bc03e97fa27138e787042378c82dd9ecd6b05
Cythilya Tang <cythilya@gmail.com>
2018-02-17 10:41:56 +0800

    [mod] 修正縮排

--- test/e2e/member/testLogin.js
+++ test/e2e/member/testLogin.js
@@ -2,15 +2,15 @@ module.exports = {
   // '@disabled': true,
   'Ruten Login': browser => {
     browser
-    .url('https://member.ruten.com.tw/user/login.htm')
-    .waitForElementVisible('body', 1000)
-    .useXpath()
-    .setValue('//input[@id="userid"]', 'nightwatch101')
-    .setValue('//input[@id="userpass"]', 'nightwatch101')
-    .useCss()
-    .click('#btnLogin')
-    .pause(1000)
-    .assert.urlContains('http://www.ruten.com.tw/')
-    .end();
+      .url('https://member.ruten.com.tw/user/login.htm')
+      .waitForElementVisible('body', 1000)
+      .useXpath()
+      .setValue('//input[@id="userid"]', 'nightwatch101')
+      .setValue('//input[@id="userpass"]', 'nightwatch101')
+      .useCss()
+      .click('#btnLogin')
+      .pause(1000)
+      .assert.urlContains('http://www.ruten.com.tw/')
+      .end();
   }
 };
