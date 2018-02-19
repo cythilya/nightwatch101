6526db23a628cc34d201482b8b6d800413f2d66c
Cythilya Tang <cythilya@gmail.com>
2018-02-19 22:58:52 +0800

    [add] assert example

--- test/e2e/class/testMainCategoryAssertSimpleExample3.js
+++ test/e2e/class/testMainCategoryAssertSimpleExample3.js
@@ -9,7 +9,7 @@ module.exports = {
       .assert.attributeEquals('.rt-site-search-submit', 'type', 'submit') 
       // 檢視屬性（attribute）是否 包含 指定的值
       .assert.attributeContains('.rt-site-search-submit', 'class', 'rt-button')
-      // 檢視 DOM element 的 CSS 屬性是否含有某個值
+      // 檢視 DOM element 的 CSS 屬性是否等於指定的值
       .assert.cssProperty('.rt-site-search-submit', 'min-height', '24px')
       // 檢視 DOM element 是否含有某個 CSS class，與 cssClassNotPresent 相反 
       .assert.cssClassPresent('.rt-site-search-submit', 'rt-button')
