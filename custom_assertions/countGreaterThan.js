2920d7da5ccfa547e2484cfd5306a19e9b0579ee
Cythilya Tang <cythilya@gmail.com>
2018-03-03 21:56:00 +0800

    [add] custom assertion: testing if the count of the element is greater than the expected number

---
+++ custom_assertions/countGreaterThan.js
@@ -0,0 +1,21 @@
+exports.assertion = function(selector, count) {
+  this.message =  `Testing if the element <${selector}> has count greater than ${count}`;
+  
+  this.expected = count;
+  
+  this.value = function(result) {
+    return result.value
+  };
+  
+  this.pass = function(value) {
+    return value > this.expected
+  };
+
+  this.command = function(cb) {
+    return this.api.execute(function(selector) {
+      return document.querySelectorAll(selector).length;
+    }, [selector], function(res) {
+      cb.call(this, res);
+    }.bind(this));
+  };
+};
