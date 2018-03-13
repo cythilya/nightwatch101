7a78a35559fc28f5c3e5a9d8376b3a2697728b05
Cythilya Tang <cythilya@gmail.com>
2018-03-13 22:37:57 +0800

    [add] 判斷可見或存在的範例

---
+++ test/e2e/class/testVisibleOrPresent.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'Guess visible or present?': browser => {
+    browser
+      .rtUrl('class', 'category/main?0023')
+      .isVisible('.rt-ad-heading') // 可見，沒有斷言，無法顯示測試結果
+      .isVisible('.rt-ad-text-only .rt-ad-item.hide') // 不可見，沒有斷言，無法顯示測試結果
+      .verify.visible('.rt-ad-heading') // 可見
+      .verify.elementPresent('.rt-ad-heading') // 可見，必為存在
+      .verify.hidden('.rt-ad-text-only .rt-ad-item.hide') // 不可見，隱藏
+      .verify.elementPresent('.rt-ad-text-only .rt-ad-item.hide') // 存在，但隱藏
+      .verify.elementNotPresent('.abcdef') // 不存在
+      .end()
+  }
+}
