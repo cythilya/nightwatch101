7030993c8c761d8f07c11c34ce540c5dbe9ed147
Cythilya Tang <cythilya@gmail.com>
2018-04-02 21:21:46 +0800

    [mod] 使用客製化指令打開網址、修改註解

--- test/e2e/class/testMainCategoryAssertSimpleExample2.js
+++ test/e2e/class/testMainCategoryAssertSimpleExample2.js
@@ -1,9 +1,9 @@
+// Assert: urlEquals, urlContains, visible, elementPresent
 module.exports = {
   '@tags': ['class'],
-  // urlEquals, urlContains, visible, elementPresent
   'Test Main Category Page': browser => {
     browser
-      .url('http://class.ruten.com.tw/category/main?0008')
+      .rtUrl('class', 'category/main?0008') // 目前網址等於特定字串？
       .waitForElementVisible('body')
       .assert.urlEquals('http://class.ruten.com.tw/category/main?0008') // 檢視目前網址是否 等於 特定字串
       .assert.urlContains('/category/main') // 檢視目前網址是否 包含 特定字串
