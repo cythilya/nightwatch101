a6f7f65b64b6019386c67b15a672a774491ed4cc
Cythilya Tang <cythilya@gmail.com>
2018-04-02 20:58:52 +0800

    [mod] 使用客製化指令打開網址、加入展開螢幕指令

--- test/e2e/testSaveScreenshot.js
+++ test/e2e/testSaveScreenshot.js
@@ -1,8 +1,9 @@
 module.exports = {
-  'Demo Test': browser => {
+  'Test Save Screenshot': browser => {
     browser
-      .url('http://www.ruten.com.tw/')
-      .saveScreenshot('./screenshots/index.png')
+      .rtUrl('www')
+      .maximizeWindow() // 展開到螢幕最大寬度
+      .saveScreenshot('./screenshots/index.png') // 儲存螢幕截圖
       .end()
   }
 }
