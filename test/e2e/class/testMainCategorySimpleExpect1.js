989d191a09637a931fbf87a973103c3c32c7c3ec
Cythilya Tang <cythilya@gmail.com>
2018-04-02 21:04:23 +0800

    [mod] 使用客製化指令打開網址、修改註解

--- test/e2e/class/testMainCategorySimpleExpect1.js
+++ test/e2e/class/testMainCategorySimpleExpect1.js
@@ -1,12 +1,18 @@
+// Expect 範例：text, equals, contains, matches, not
 module.exports = {
   '@tags': ['class'],
-  // text, equals, contains, matches, not
   'Test Main Category Page 1': browser => {
-    browser.url('http://class.ruten.com.tw/category/main?0008');
+    browser.rtUrl('class', 'category/main?0008');
     browser.waitForElementVisible('body');
-    browser.expect.element('.rt-breadcrumb-static').text.to.not.equals('Hello World!'); // 檢視 rt-breadcrumb-static 的 inner text 是否不為「Hello World!」
-    browser.expect.element('.rt-breadcrumb-static').text.to.contains('攝影機'); // 檢視 .rt-breadcrumb-static 的 inner text 是否包含「攝影機」
-    browser.expect.element('.rt-breadcrumb-static').text.to.matches(/^([^0-9]*)$/); // 檢視 .rt-breadcrumb-static 的 inner text 是否不含數字
+ 
+    // 文字內容不為 Hello World？
+    browser.expect.element('.rt-breadcrumb-static').text.to.not.equals('Hello World!'); 
+
+    // 文字內容包含「攝影機」？
+    browser.expect.element('.rt-breadcrumb-static').text.to.contains('攝影機'); 
+
+    // 文字內容不含數字？
+    browser.expect.element('.rt-breadcrumb-static').text.to.matches(/^([^0-9]*)$/); 
     browser.end();
   }
 }
