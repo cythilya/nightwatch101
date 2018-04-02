438948f133624450c3ae0f8e2acdd0c326dbebdf
Cythilya Tang <cythilya@gmail.com>
2018-04-02 20:34:27 +0800

    [mod] 改用客製化指令登入、修改註解

--- test/e2e/member/testDesktopLogin.js
+++ test/e2e/member/testDesktopLogin.js
@@ -1,8 +1,8 @@
 module.exports = {
   'Ruten Desktop Login': browser => {
     browser
-      .url('https://member.ruten.com.tw/user/login.htm') // 打開指定網址
-      .waitForElementPresent('body', 1000) // 確認 DOM Element 載入完成
+      .rtUrl('member', 'user/login.htm') // 打開指定網址
+      .waitForElementPresent('body', 1000) // body 存在? 確認 DOM Element 載入完成
       .setValue('#userid', 'nightwatch101') // 對 input text 設値或模擬連續打字，字串格式為 utf-8
       .setValue('#userpass', 'nightwatch101')
       .click('#btnLogin') // 點擊送出按鈕
