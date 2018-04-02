425b72e0839f253c3427bc7fdb1968eedfd4ef2c
Cythilya Tang <cythilya@gmail.com>
2018-04-02 21:14:38 +0800

    [mod] 使用客製化指令打開網址、修改註解

--- test/e2e/find/testFindPusheenSimpleExpect.js
+++ test/e2e/find/testFindPusheenSimpleExpect.js
@@ -1,13 +1,19 @@
+// Expect 範例：enabled, selected, value
 module.exports = {
   '@tags': ['find'],
-  // enabled, selected, value
-  // 7-11 取貨付款
-  'Test Find Pusheen Page': browser => {
-    browser.url('https://find.ruten.com.tw/s/?area=0&cateid=0015&payway=7&q=pusheen&shipfee=all');
+  'Test Find Pusheen Page': browser => { // 7-11 取貨付款
+    browser.rtUrl('find', 's/?area=0&cateid=0015&payway=7&q=pusheen&shipfee=all');
     browser.waitForElementVisible('body');
+
+    // 選取？
     browser.expect.element('.payment > li:nth-child(6) > label > input[type="checkbox"]').to.be.selected;
+    
+    // 值為 pusheen？
     browser.expect.element('#searchInput').to.have.value.that.equals('pusheen');
+
+    // 啟用？
     browser.expect.element('#searchButton').to.be.enabled;
+    
     browser.end();
   }
 }
